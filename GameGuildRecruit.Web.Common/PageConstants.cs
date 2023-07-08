c957927de15c61774012bea6f1de25d8cf88b2e0
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2023-07-08 11:17:41 +0300

    chaining views and entities nums per page

--- GameGuildRecruit.Web.Common/PageConstants.cs
+++ GameGuildRecruit.Web.Common/PageConstants.cs
@@ -4,6 +4,6 @@ namespace GameGuildRecruit.Web.Common
     public static class PageConstants
     {
         public const int DefaultPage = 1;
-        public const int EntitiesPerPage = 3;
+        public const int EntitiesPerPage = 2;
     }
 }
