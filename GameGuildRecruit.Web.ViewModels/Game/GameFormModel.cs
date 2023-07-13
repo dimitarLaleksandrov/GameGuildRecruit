88056eb95b0e6a4dcd286f531b0393e04c3d4394
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2023-07-13 13:51:58 +0300

    adding game vew and form model

---
+++ GameGuildRecruit.Web.ViewModels/Game/GameFormModel.cs
@@ -0,0 +1,33 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using static GameGuildRecruit.Web.Common.EntityValidations.Game;
+
+namespace GameGuildRecruit.Web.ViewModels.Game
+{
+    public class GameFormModel
+    {
+        public GameFormModel()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+
+        [Key]
+        public Guid Id { get; set; }
+
+
+        [Required]
+        [StringLength(GameNameMaxLength, MinimumLength = GameNameMinLength)]
+        [Display(Name = "GameName")]
+        public string GameName { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "GameSlideImageURL")]
+        public string GameSlideImageURL { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "GameLogoImageURL")]
+        public string GameLogoImageURL { get; set; } = null!;
+
+    }
+}
---
+++ GameGuildRecruit.Web.ViewModels/Game/GameViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace GameGuildRecruit.Web.ViewModels.Game
+{
+    public class GameViewModel
+    {
+        public GameViewModel()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string GameName { get; set; } = null!;
+
+        public string GameSlideImageURL { get; set; } = null!;
+
+        public string GameLogoImageURL { get; set; } = null!;
+    }
+}
