211b6cf1bd6c57af75713cb0c0c31b58ac4ba452
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2023-07-28 08:34:58 +0300

    adding new check in WoW contact page controller

--- GameGuildRecruit.Web/Controllers/PagesController.cs
+++ GameGuildRecruit.Web/Controllers/PagesController.cs
@@ -159,6 +159,20 @@ namespace GameGuildRecruit.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> WoWContacts(Guid id)
         {
+            var gameName = "WorldOfWarcraft";
+
+            var gamePage = await pageService.GetGameByNameAsync(gameName);
+
+            if (gamePage == null)
+            {
+                return RedirectToAction("PageError", "Errors");
+            }
+
+            if (gamePage.IsGameHasView == false)
+            {
+                return RedirectToAction("PageError", "Errors");
+            }
+
             try
             {
                 var contactsModels = await pageService.GetUserContactsByIdAsync(id);
