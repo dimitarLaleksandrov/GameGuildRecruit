006ee8a7a3ba17db37a02cc368f30be19839f846
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2023-08-04 09:49:37 +0300

    adding Test project + nuget pakege

---
+++ GameGuildRecruitTests/GuildRecruitUserTest.cs
@@ -0,0 +1,35 @@
+﻿using GameGuildRecruit.Web.Data;
+using GameGuildRecruit.Web.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameGuildRecruitTests
+{
+    internal class GuildRecruitUserTest
+    {
+
+        private GuildRecruitUserService userService;
+
+        private readonly GameGuildRecruitDbContext dbContext;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            userService = new GuildRecruitUserService(dbContext);
+        }
+
+
+
+        [Test]
+
+        public void 
+
+
+
+    }
+}
