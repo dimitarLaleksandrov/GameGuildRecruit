5918cbba8c78ae358b4c4db5adcf89ec1f55e188
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2023-08-06 13:51:02 +0300

    adding unit test

---
+++ GameGuildRecruitTests/UnitTests/GuildRecruitUserServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameGuildRecruit.Web.Services;
+using GameGuildRecruit.Web.Services.Interfaces;
+
+namespace GameGuildRecruit.Tests.UnitTests
+{
+
+    [TestFixture]
+    public class GuildRecruitUserServiceTests : UnitTestsBase
+    {
+        private IGuildRecruitUserService _guildRecruitUserService;
+
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _guildRecruitUserService = new GuildRecruitUserService(_data);
+
+        }
+
+    }
+
+  
+
+}
