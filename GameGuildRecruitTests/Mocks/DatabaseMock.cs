aff8fd97a5edd7c177417151fc0b6dec37353c30
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2023-08-06 09:30:23 +0300

    adding mocks  Db in the test

---
+++ GameGuildRecruitTests/Mocks/DatabaseMock.cs
@@ -0,0 +1,28 @@
+﻿using GameGuildRecruit.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameGuildRecruit.Tests.Mocks
+{
+    public class DatabaseMock
+    {
+
+        public static GameGuildRecruitDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<GameGuildRecruitDbContext>()
+                    .UseInMemoryDatabase("GameGuildRecruitInMemoryDb" + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+
+                return new GameGuildRecruitDbContext(dbContextOptions, false);
+
+            }
+        }
+    }
+}
